dfc8c237407c815aa42bcead3d8d707e2a1d659e
Seth Battin <seth.battin@gmail.com>
2016-01-03 03:25:08 -0600

    begin separating connection, weird timing error

--- web/script.js
+++ web/script.js
@@ -4,9 +4,13 @@
 
 window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
 
-var rtcpc = function(connectionName){
+var instance = 0;
+
+var rtcpc = function(connection){
     
-    this.connection = 
+    this.i = instance++;
+    
+    this.connection = connection;
     
     var pcServers = null;
     var pcConstraint = null;
@@ -17,9 +21,10 @@ var rtcpc = function(connectionName){
     var self = this;
     var channelReceive = null;
     this.peerConnection.ondatachannel = function(e){
+        console.log("channel");
         channelReceive = e.channel;
         channelReceive.onmessage = function(event){
-            console.log(connectionName + ' received: "' + event.data + '".');
+            console.log(self.i + ' received: "' + event.data + '".');
         };
     };
     
@@ -30,31 +35,74 @@ var rtcpc = function(connectionName){
         console.error("create answer error", error);
     };
 
-    this.call = function(answerer) {
-        
+    this.call = function() {
+        var self = this;
         var onCreated = function(description){
             self.peerConnection.setLocalDescription(description);
-            answerer.peerConnection.setRemoteDescription(description);
-            answerer.answer(self);
+            self.connection.offer = description;
         };
         
         this.peerConnection.createOffer(onCreated, createOfferError);
     };
-    this.answer = function(caller){
+    this.answer = function(){
+        if (!this.connection.offer){
+            return;
+        }
+
+        var self = this;
         this.peerConnection.onicecandidate = function(event){
             if(event.candidate) {
-                caller.peerConnection.addIceCandidate(event.candidate);
+                self.connection.candidate = event.candidate;
             }
         };
-        var self = this;
         var onAnswer = function(description){
             self.peerConnection.setLocalDescription(description);
-            caller.peerConnection.setRemoteDescription(description);
+            self.connection.answer = description;
         };
+        
+        this.peerConnection.setRemoteDescription(this.connection.offer);
         this.peerConnection.createAnswer(onAnswer, createAnswerError)
+    };
+    this.complete = function(){
+        if (!this.connection.answer || !this.connection.candidate){
+            return;
+        }
+        this.peerConnection.setRemoteDescription(this.connection.answer);
+        this.peerConnection.addIceCandidate(this.connection.candidate);
+    };
+};
+
+var ConnectionModel = function(name){
+    this.name = name;
+    this.offer = '';
+    this.answer = '';
+    this.candidate = '';
+};
+
+
+var connection = new ConnectionModel('abcd');
+var conn1 = new rtcpc(connection);
+var conn2 = new rtcpc(connection);
+
+conn1.call(conn2);
+
+var answerPoll = 0;
+var createPoll = 0;
+
+var answer = function(){
+    console.log("answer", connection.answer, connection.candidate);
+    if (connection.answer && connection.candidate){
+        clearInterval(answerPoll);
+        conn1.complete();
+    }
+};
+var create = function(){
+    console.log("create");
+    if (connection.offer){
+        clearInterval(createPoll);
+        conn2.answer(conn1);
+        answerPoll = setInterval(answer, 20);
     }
 };
 
-var conn1 = new rtcpc('conn1');
-var conn2 = new rtcpc('conn2');
-conn1.call(conn2);
+createPoll = setInterval(create, 400);
