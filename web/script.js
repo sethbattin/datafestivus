62ef6ebf40d0cd336988284099ad97ad64853db8
Seth Battin <seth.battin@gmail.com>
2016-01-04 00:02:02 -0600

    relocate startup logic to page script

--- web/script.js
+++ web/script.js
@@ -7,35 +7,34 @@ window.RTCPeerConnection =
     window.mozRTCPeerConnection || 
     window.webkitRTCPeerConnection;
 
-var RTCConnection = function(id, connection) {
-
-    this.i = id;
-    var _connection = connection;
-
+var RTCData = function(id, _connection) {
+    
+    this.id = id;
     var pcServers = null;
     var pcConstraint = null;
 
     this.peerConnection = new RTCPeerConnection(pcServers, pcConstraint);
-    this.channel = this.peerConnection.createDataChannel('player' + this.i, null);
+    this.channel = this.peerConnection.createDataChannel('player' + this.id, null);
 
     var self = this;
     var channelReceive = null;
     this.peerConnection.onicecandidate = function (event) {
         if (event.candidate) {
-            _connection.addCandidate(event.candidate, self.i);
+            _connection.addCandidate(event.candidate, self.id);
         }
     };
     this.peerConnection.ondatachannel = function (e) {
-        console.log(self.i + " channel");
+        _connection.setReady(true);
+        console.log(self.id + " channel");
         channelReceive = e.channel;
         channelReceive.onmessage = function (event) {
-            console.log(self.i + ' received: "' + event.data + '".');
+            console.log(self.id + ' received: "' + event.data + '".');
         };
     };
 
 };
 
-RTCConnection.prototype.offer = function(connection) {
+RTCData.prototype.offer = function(connection) {
     var self = this;
     var onCreated = function(description){
         self.peerConnection.setLocalDescription(description);
@@ -47,7 +46,7 @@ RTCConnection.prototype.offer = function(connection) {
     
     this.peerConnection.createOffer(onCreated, createOfferError);
 };
-RTCConnection.prototype.answer = function(connection){
+RTCData.prototype.answer = function(connection){
     if (!connection.offer){
         return;
     }
@@ -64,51 +63,80 @@ RTCConnection.prototype.answer = function(connection){
     this.peerConnection.setRemoteDescription(connection.offer);
     this.peerConnection.createAnswer(onAnswer, createAnswerError)
 };
-RTCConnection.prototype.complete = function(connection){
-    if (!connection.answer || !connection.candidates[this.i].length){
+RTCData.prototype.complete = function(connection){
+    if (!connection.answer || !connection.candidates[this.id].length){
         return;
     }
     this.peerConnection.setRemoteDescription(connection.answer);
-    var iceCandidates = connection.getOtherCandidates(this.i);
+    var iceCandidates = connection.getOtherCandidates(this.id);
     for (var i in iceCandidates){
         this.peerConnection.addIceCandidate(iceCandidates[i]);
     }
 };
 
 var ConnectionModel = function(name) {
+    
+    // this is used for identification only
     this.name = name;
+
+    // these fields are serialized
     this.offer = '';
     this.answer = '';
     this.candidates = [];
-    this.offerer = null;
-    this.answerer = null;
+        
+    // events
+    this.onoffer = function(){};
+    this.onanswer = function(){};
+    this.onaddcandidate = function(){};
+    this.onready = function(){};
+    this.onunready = function(){};
+    
 };
-ConnectionModel.prototype.init = function(offerer){
-    this.offerer = offerer;
-    this.offerer.offer(this);
-};
-ConnectionModel.prototype.respond = function(answerer){
-    this.answerer = answerer;
-    if (this.offer){
-        this.answerer.answer(this);
-    }
-};
-ConnectionModel.prototype.checkComplete = function() {
-    if (this.offerer && this.answer){
-        var candidates = this.getOtherCandidates(this.offerer.i);
-        if (candidates.length){
-            this.offerer.complete(this);
+ConnectionModel.prototype.init = function(name){
+    var offerer = new RTCData(name, this);
+    offerer.offer(this);
+
+    var checkComplete = function(conn) {
+        if (conn.answer){
+            var candidates = conn.getOtherCandidates(name);
+            if (candidates.length){
+                offerer.complete(conn);
+            }
         }
+    };
+
+    this.onaddcandidate = checkComplete;
+    this.onanswer = checkComplete;
+    
+    return offerer;
+};
+ConnectionModel.prototype.respond = function(name){
+    var answerer = new RTCData(name, this);
+    
+    this.onoffer = function(conn){
+        if (conn.offer) {
+            answerer.answer(conn);
+        }
+    };
+    
+    return answerer;
+};
+ConnectionModel.prototype.setReady = function(val){
+    this.ready = val;
+    if (this.ready){
+        this.onready(this);
+    } else {
+        this.onunready(this);
     }
 };
 
 ConnectionModel.prototype.setAnswer = function(answer) {
     this.answer = answer;
-    this.checkComplete();
+    this.onanswer(this);
 };
 ConnectionModel.prototype.setOffer = function(offer) {
     this.offer = offer;
-    this.answerer.answer(this);
+    this.onoffer(this);
 };
 
 ConnectionModel.prototype.addCandidate = function(candidate, id){
@@ -116,7 +144,7 @@ ConnectionModel.prototype.addCandidate = function(candidate, id){
         this.candidates[id] = [];
     }
     this.candidates[id].push(candidate);
-    this.checkComplete();
+    this.onaddcandidate(this);
 };
 ConnectionModel.prototype.getOtherCandidates = function(id){
     var result = [];
@@ -128,7 +156,3 @@ ConnectionModel.prototype.getOtherCandidates = function(id){
     }
     return result;
 };
-
-var connection = new ConnectionModel('abcd');
-connection.init(new RTCConnection(1, connection));
-connection.respond(new RTCConnection(2, connection));
